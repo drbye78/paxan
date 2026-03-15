1b1e48fa5fdc8d1a8b085875d3626af11e3cae5a
Marusin Dmitry <drbye@mail.ru>
2026-03-16 00:11:38 +0300

    fix: upgrade Hilt to 2.50 for better KSP support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,8 +78,8 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
 
     // Hilt
-    implementation("com.google.dagger:hilt-android:2.48.1")
-    ksp("com.google.dagger:hilt-android-compiler:2.48.1")
+    implementation("com.google.dagger:hilt-android:2.50")
+    ksp("com.google.dagger:hilt-android-compiler:2.50")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
 
     // Network
